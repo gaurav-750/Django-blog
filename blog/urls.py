741204c92a5359cac3cf8d2d51fa047c00b86736
Gaurav <gauravsomani52750@gmail.com>
2022-12-24 15:31:52 +0530

    blog detail page done

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, BlogDetailView
 
 urlpatterns = [
-    path('', BlogView.as_view(), name='home')
+    path('', BlogView.as_view(), name='home'),
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,  DetailView
 from .models import Post
 
 
@@ -8,3 +8,9 @@ class BlogView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'blogs'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'blog'  # the object which we r sending
