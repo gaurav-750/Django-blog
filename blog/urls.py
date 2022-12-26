5ca67fdea865719f6ab3e7a4f29733a97b36cdb5
Gaurav <gauravsomani52750@gmail.com>
2022-12-26 10:35:04 +0530

    delete blog post feature added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='home'),
     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit')
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView,  DetailView, CreateView, UpdateView
-from .models import Post
-
+from django.views.generic import \
+    ListView,\
+    DetailView, \
+    CreateView,\
+    UpdateView, \
+    DeleteView
 from django.urls import reverse, reverse_lazy
+
+from .models import Post
 
 
 # Create your views here.
@@ -28,3 +33,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
