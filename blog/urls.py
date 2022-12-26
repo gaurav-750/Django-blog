cc476a2e79c607b8974e56551354b033244d41ff
Gaurav <gauravsomani52750@gmail.com>
2022-12-26 10:21:12 +0530

    edit blog post feature added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogView, BlogDetailView, BlogCreateView
+from .views import BlogView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='home'),
     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/', BlogCreateView.as_view(), name='post_new')
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,  DetailView, CreateView
+from django.views.generic import ListView,  DetailView, CreateView, UpdateView
 from .models import Post
 
 from django.urls import reverse, reverse_lazy
@@ -22,3 +22,9 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
