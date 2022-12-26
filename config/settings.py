a3a883a44770b98f88d00bb585e7ed28ff37e5d6
Gaurav <gauravsomani52750@gmail.com>
2022-12-26 11:30:24 +0530

    login and logout using django.contrib.auth app

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
