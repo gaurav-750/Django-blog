c3a87198d231ff8091983e9540ea5d1f22edc585
Gaurav <gauravsomani52750@gmail.com>
2022-12-27 09:34:34 +0530

    deploying to railway

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView
 
 # Create your views here.
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm  # built in Django form to register User
+    # *after signup, user'll be redirected to Login page
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%e9sm!du%$)vml#82p-350jb!6d$1ed(^byb@_^0#+qw9ic114
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # to tell our django about static files
 
